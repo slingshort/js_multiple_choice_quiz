53204b03982fa1c68a144dd2f7d274a63d20637d
slingshort <salina.myat@gmail.com>
2022-09-26 15:19:24 +1000

    minor bug fixes

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -4,18 +4,27 @@ var score = localStorage.getItem("score")
 console.log(initials)
 console.log(score)
 
+// creating a var that combines initials and score to store as an array object in local storage
 highscoreITEM = initials + " " + score
 
+// create an array to push highscoreITEM array objects to
 var highscoreSTORAGE = ["test",]
-
 highscoreSTORAGE.push(highscoreITEM)
 
+// setting in local storage
 localStorage.setItem("highscoreLIST",JSON.stringify(highscoreSTORAGE))
 
 var renderLIST = JSON.parse(localStorage.getItem("highscoreLIST"))
 console.log(renderLIST)
-// localStorage.setItem("highscoreITEM",highscoreITEM )
+
+// creating list element and changing text content hold highscoreRESULTS
+var highscoreRESULTS = document.createElement("li")
+highscoreRESULTS.textContent = renderLIST
+
+// targeting area of HTML to append list to
+var highscoreLIST = document.getElementById("highscoreLIST")
+highscoreLIST.appendChild(highscoreRESULTS)
 
 
-// document.getElementById("highscoreLIST").appendChild(highscoreITEM)
+
 
